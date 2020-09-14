9747d00259faff1e8ab62c93f9f0425876a043b5
AdarshRevankar <adarsh_revankar@live.com>
2020-09-14 13:01:18 +0530

    FaceAuth: Heroku Compatibility

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import base64
 import io
+import os
 
 from flask import Flask, request, jsonify
 from flask import render_template
@@ -43,4 +44,5 @@ def capture():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
