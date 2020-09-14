7a6bee3be583b81b3c6345a402cabc9cef0f6428
AdarshRevankar <adarsh_revankar@live.com>
2020-09-14 21:35:26 +0530

    FaceAuth: Page Redirection

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import base64
 import io
-import os
 
 from flask import Flask, request, jsonify
 from flask import render_template
 from PIL import Image
 
-from login import input_verification, create_new_account, check_username_existence
+from login import check_credintial, input_verification, create_new_account, check_username_existence
 
 app = Flask(__name__)
 
@@ -27,6 +26,29 @@ def register():
         # Create A new Account
         create_new_account(uname, password)
         return jsonify({'register': 'Registration Successful'})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    uname = str(request.values['username'])
+    password = str(request.values['password'])
+
+    # Verify the user
+    if check_credintial(uname, password):
+        html = open('templates/profile.html').read()
+        html = html.replace('USERNAME', uname)
+        return jsonify({'login': True, 'message': html})
+    else:
+        if check_username_existence(uname):
+            return jsonify({'login': False, 'message': 'Wrong password'})
+        return jsonify({'login': False, 'message': 'User Not Found'})
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def showprofile():
+    uname = str(request.values['username'])
+    password = str(request.values['password'])
+    return render_template('profile.html', username=uname)
 
 
 @app.route('/capture', methods=['POST'])
---
+++ profile.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, uname, password):
+        self.uname = uname
+        self.password = password
+        self.capture_model()
+
+    def capture_model(self):
+        pass
